6f25d134f6e9702f4103cbf623a44552e744bad2
Aarav8 <70571868+Aarav8@users.noreply.github.com>
2022-01-26 18:09:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function take_snapshot()
 
 console.log("ml5.versionn is ",ml5.version);
 
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/MDxHRPHYk/",modelLoaded());
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/9AdTddVZW/",modelLoaded());
 
 function modelLoaded()
 {
